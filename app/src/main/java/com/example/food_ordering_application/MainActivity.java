28b2bb719aa270facd74fc31e125c5eca32d84a5
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-09-10 14:10:44 +0530

    Empty Project For food ordering App

---
+++ app/src/main/java/com/example/food_ordering_application/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.food_ordering_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
