2edcbf983214e343dfc72eda8a889add5bbccd1c
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-09-22 21:00:17 +0530

    Register Page User Interface Created

--- app/src/main/java/com/example/food_ordering_application/RegisterFragment.java
+++ app/src/main/java/com/example/food_ordering_application/RegisterFragment.java
@@ -9,14 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link RegisterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class RegisterFragment extends Fragment
+{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,8 +27,7 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment()
-    {
+    public RegisterFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +37,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment RegisterFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static RegisterFragment newInstance(String param1, String param2) {
+        RegisterFragment fragment = new RegisterFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,18 +60,15 @@ public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        EditText email = view.findViewById(R.id.email);
-        EditText password = view.findViewById(R.id.password);
-
-        Button loginButton = view.findViewById(R.id.loginButton);
-        TextView txtregister = view.findViewById(R.id.txtRegister);
-
-
+        EditText userName = view.findViewById(R.id.userName);
+        EditText regEmail = view.findViewById(R.id.regEmail);
+        EditText regPassword = view.findViewById(R.id.regPassword);
+        EditText regRePassword = view.findViewById(R.id.regRePassword);
+        Button registerButton = view.findViewById(R.id.registerButton);
 
 
         return view;
